39d1806c151402fb9d510e21e1c56cac9b02fce6
jordi <mjfarthi@ncsu.edu>
2022-08-29 19:02:51 -0400

    First Commit

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// This app is deployed on heroku at https://protected-castle-57271.herokuapp.com/
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
